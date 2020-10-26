5720f90508dc0a30ea129f847008dd6efea354bd
elvcastelo <eliaslvcastelo@gmail.com>
2020-10-26 01:19:08 -0300

    Mudança do tema (cores) e pré-visualização do editor

--- src/components/ExamEditor/index.js
+++ src/components/ExamEditor/index.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import MathContext from '../MathContext';
+import Navbar from '../Navbar';
+import examData from '../Exam/data/exams.json';
+import './index.css';
 
 function ExamEditor() {
+
+    const [textPreview, setTextPreview] = useState("$$ \\int x^2 dx = \\frac{x^3}{3} $$")
+    try {
+        console.log(JSON.parse(`{"a": "${textPreview.replace("\\", "\\\\")}"}`))
+    } catch { }
+
     return (
-        <div className="exam-editor-holder">
-            
-        </div>
+        <>
+            <Navbar />
+            <div className="exam-editor-holder">
+                <h1 className="exam-editor-title">Editor de Questões</h1>
+                <p className="exam-editor-subtitle">Ferramenta para facilitar a adição e edição de questões.</p>
+                <textarea className="exam-editor-textarea" onChange={(e) => setTextPreview(e.target.value)}>{textPreview}</textarea>
+                <div className="exam-editor-preview">
+                    <p>Pré-visualização:</p>
+                    <MathContext content={String.raw`${textPreview}`} />
+                </div>
+            </div>
+        </>
     )
 }
 
--- src/components/MathContext/index.js
+++ src/components/MathContext/index.js
@@ -4,7 +4,6 @@ function MathContext(props) {
     return(
         <MathJax.Context 
             input='tex'
-            onLoad={() => console.log("Script carregado!")}
             onError={ (MathJax, error) => {
                 console.log("Foi encontrado um erro.")
             }}
